45d3cefb6b5042097b68837256ac99ca5a09f57e
Júlia Yoshida <juliayosh@gmail.com>
2024-06-26 15:14:14 -0300

    feat#82: adding drag and drop to item swap

--- Unity-Project/Assets/Scripts/InventoryController.cs
+++ Unity-Project/Assets/Scripts/InventoryController.cs
@@ -2,29 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class InventoryController : MonoBehaviour
 {
-    public Objects[] slots; 
-    public Image[] slotImage;     
+    public Objects[] slots;
+    public Image[] slotImage;
     private GameObject nearbyObject;
     public GameObject inventoryPanel;
+    private GameObject draggedItem;
 
-    private int selectedSlotIndex = -1; 
-
-    void Start()
+    private void Start()
     {
         slots = new Objects[slotImage.Length];
 
         for (int i = 0; i < slotImage.Length; i++)
         {
-            int index = i; 
-            slotImage[i].gameObject.AddComponent<Button>(); 
-            slotImage[i].GetComponent<Button>().onClick.AddListener(() => OnSlotClick(index)); 
+            int index = i;
+            slotImage[i].gameObject.AddComponent<Button>();
+            SlotDragHandler dragHandler = slotImage[i].gameObject.AddComponent<SlotDragHandler>();
+            SlotDropHandler dropHandler = slotImage[i].gameObject.AddComponent<SlotDropHandler>();
+            dragHandler.Initialize(this, index);
+            dropHandler.Initialize(this, index);
         }
     }
 
-    void Update()
+    private void Update()
     {
         if (inventoryPanel.activeInHierarchy) return;
 
@@ -39,6 +42,7 @@ public class InventoryController : MonoBehaviour
                     {
                         slots[i] = objectType.objectType;
                         slotImage[i].sprite = objectType.objectType.itemSprite;
+                        slotImage[i].color = Color.white; 
                         Destroy(nearbyObject);
                         nearbyObject = null;
                         break;
@@ -48,7 +52,7 @@ public class InventoryController : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Object"))
         {
@@ -56,7 +60,7 @@ public class InventoryController : MonoBehaviour
         }
     }
 
-    void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Object"))
         {
@@ -67,22 +71,7 @@ public class InventoryController : MonoBehaviour
         }
     }
 
-    void OnSlotClick(int index)
-    {
-        if (selectedSlotIndex == -1)
-        {
-            selectedSlotIndex = index;
-            Debug.Log("Slot selecionado: " + index);
-        }
-        else
-        {
-            SwapItems(selectedSlotIndex, index);
-            Debug.Log("Itens trocados: " + selectedSlotIndex + " com " + index);
-            selectedSlotIndex = -1; 
-        }
-    }
-
-    void SwapItems(int index1, int index2)
+    public void SwapItems(int index1, int index2)
     {
         Objects temp = slots[index1];
         slots[index1] = slots[index2];
@@ -91,5 +80,80 @@ public class InventoryController : MonoBehaviour
         Sprite tempSprite = slotImage[index1].sprite;
         slotImage[index1].sprite = slotImage[index2].sprite;
         slotImage[index2].sprite = tempSprite;
+        slotImage[index1].color = slots[index1] != null ? Color.white : Color.clear;
+        slotImage[index2].color = slots[index2] != null ? Color.white : Color.clear;
+    }
+}
+
+public class SlotDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private InventoryController inventoryController;
+    public int slotIndex; 
+    private Image itemImage;
+    private GameObject draggedItem;
+
+    private Color originalColor;
+
+    public void Initialize(InventoryController controller, int index)
+    {
+        inventoryController = controller;
+        slotIndex = index;
+        itemImage = GetComponent<Image>();
+        originalColor = itemImage.color; 
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (itemImage.sprite == null) return;
+
+        draggedItem = new GameObject("DraggedItem");
+        draggedItem.transform.SetParent(transform.root, false);
+        draggedItem.transform.SetAsLastSibling();
+        Image draggedItemImage = draggedItem.AddComponent<Image>();
+        draggedItemImage.sprite = itemImage.sprite;
+        draggedItemImage.raycastTarget = false;
+        RectTransform rectTransform = draggedItem.GetComponent<RectTransform>();
+        rectTransform.sizeDelta = itemImage.GetComponent<RectTransform>().sizeDelta;
+        draggedItemImage.color = itemImage.color;
+        itemImage.color = new Color(originalColor.r, originalColor.g, originalColor.b, 0f);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (draggedItem != null)
+        {
+            draggedItem.transform.position = Input.mousePosition;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (draggedItem != null)
+        {
+            Destroy(draggedItem);
+        }
+        itemImage.color = originalColor;
+    }
+}
+
+public class SlotDropHandler : MonoBehaviour, IDropHandler
+{
+    private InventoryController inventoryController;
+    private int slotIndex;
+
+    public void Initialize(InventoryController controller, int index)
+    {
+        inventoryController = controller;
+        slotIndex = index;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        SlotDragHandler draggedSlot = eventData.pointerDrag.GetComponent<SlotDragHandler>();
+        if (draggedSlot != null)
+        {
+            int draggedSlotIndex = draggedSlot.slotIndex;
+            inventoryController.SwapItems(draggedSlotIndex, slotIndex);
+        }
     }
 }
